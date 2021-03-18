37299ef48ec2e445a6ca3e16f06235c73a6af7ea
anton-domenikov <79518417+anton-domenikov@users.noreply.github.com>
2021-03-18 10:43:04 +0200

    changed mongoose to connect to cloud, not localhost

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 4040,
-    DB_URI: `mongodb://localhost/js-backend-paragon`,
+    DB_URI: `mongodb+srv://admin:atlasadmin4i123@cluster0.5t60k.mongodb.net/database?retryWrites=true&w=majority`,
     SALT_ROUNDS: 10,
     SECRET: 'MNOGOQKASOLBATE',
     COOKIE_NAME: 'TOKEN'
